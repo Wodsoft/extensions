453d8a9580b35c18ba752fa647da81f0266928d6
Kation <kation@outlook.com>
2020-06-23 11:42:11 +0800

    Rename

---
+++ src/Wodsoft.DotNet40.Runtime/Collections/ObjectModel/ReadOnlyCollection2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+
+namespace System.Collections.ObjectModel
+{
+    public class ReadOnlyCollection2<T> : ReadOnlyCollection<T>, IReadOnlyCollection<T>
+    {
+        public ReadOnlyCollection2(IList<T> list) : base(list) { }
+    }
+}
